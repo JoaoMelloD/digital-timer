4f06d7c10b67c297e33e4325e7eed5764a15f095
JoaoMelloD <joao.delgado@edu.unipar.br>
2025-04-15 09:20:12 -0300

    Timer Funcional: No Data Formatation

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,43 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [view, setView] = useState(0);
-  const [valor, setValor] = useState(0);
-  const [on, setOn] = useState(false);
+  const [horas, setHoras] = useState(0);
+  const [minutos, setMinutos] = useState(0);
+  const [segundos, setSegundos] = useState(0);
+  const [ligar, setLigar] = useState(false)
 
-  function temporizador() {
-    const tempo = setInterval(() => {
-      const hora = new Date().toLocaleTimeString("pt-BR", {
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-      });
-      console.log(hora);
-      setValor(tempo)
-    }, 1000);
+
+  useEffect(() => {
+   
+    return (()=>{
+      setInterval(setarTimer, 1000)
+    })
+  }, [ligar]);
+
+
+  function setarTimer() {
+    let horaAtual = new Date();
+    setHoras(horaAtual.getHours());
+    setMinutos(horaAtual.getMinutes());
+    setSegundos(horaAtual.getSeconds());
   }
 
-  function Desligar() {}
-
-  function Ligar() {
-    //temporizador ligado
-    setOn(true);
-    if (on === true) {
-      temporizador();
+  function handleLigar() {
+    if (ligar === false) {
+      setLigar(true);
+    } else {
+      setLigar(false)
     }
   }
 
+
+
   return (
     <div>
-      <h1>TIMER: {valor}</h1>
-      <button onClick={Ligar}>Start</button>
-      <button onClick={Desligar}>Stop</button>
+      <h1>Timer</h1>
+      <div>{`${horas}:${minutos}:${segundos}`}</div>
+      <button onClick={handleLigar}>Ligar</button>
+
     </div>
   );
 }
